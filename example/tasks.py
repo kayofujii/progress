9b4b45b4fdf0c3d011b6128948bc5e07905b66c7
kayofujii <kayosamu1025@gmail.com>
2021-07-23 17:34:08 +0900

    first commit

---
+++ example/tasks.py
@@ -0,0 +1,10 @@
+# Create your tasks here
+
+from celery import shared_task
+
+from time import sleep
+
+@shared_task(bind=True)
+def go_to_sleep(self, duration):
+    sleep(duration)
+    return 'Done'
