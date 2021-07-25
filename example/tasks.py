0c9e4bcf0529f88a0df46abc384a632aedadc02a
kayofujii <kayosamu1025@gmail.com>
2021-07-25 16:17:15 +0900

    コード追加

--- example/tasks.py
+++ example/tasks.py
@@ -1,10 +1,14 @@
-# Create your tasks here
-
+from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 from time import sleep
 
 @shared_task(bind=True)
 def go_to_sleep(self, duration):
+    print('9')
     sleep(duration)
     return 'Done'
+
+@shared_task
+def add(x, y):
+    return x + y
--- example/views.py
+++ example/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
-from .tasks import go_to_sleep
+from celery.result import AsyncResult
+from example.tasks import go_to_sleep, add
 
 # Create your views here.
 def index(request):
     go_to_sleep.delay(5)
+    task_id = add.delay(1,3)
+    result = AsyncResult(task_id)
+    print(result.state)
+    print(result.ready)
     return render(request, 'example/index.html')
